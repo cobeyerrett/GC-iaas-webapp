503a04bca8d43c2c2624e054c780fe2b5974b83e
Adiy Qasrawi <aqasrawi@microsoft.com>
2018-01-20 00:10:58 +0000

    Updated to address region issues

--- deploy.ps1
+++ deploy.ps1
@@ -4,10 +4,37 @@
 
 Login-AzureRmAccount
 
-$subscriptionId = '3a4af7b3-b7ac-463d-9940-1d80445961a8'
- Set-AzureRmContext -SubscriptionId $subscriptionId
+$subscriptionId = '60b6165a-8669-47a2-860c-6ef475127364'
+
+$parameters=@{
+    workspaceRegion = "eastus2"
+    environmentName ="AzureCloud"
+    keyVaultName = "GCKeystoreUser1"
+    keyVaultResourceGroupName = "GCbluePrintUser1"
+    domainName = "GCblueprint.local"
+    configureSQLAO = "yes"
+    environmentPrefix = "prod"
+    numberOfWebInstances = 2
+    useExistingKek = "kek"
+    encryptionEnabled = "enabled"
+    adVMSize = "Standard_D2_v2"
+    sqlVMSize = "Standard_D2_v2"
+    webVMSize = "Standard_D2_v2"
+    mgtVMSize = "Standard_D2_v2"
+    witnessVMSize = "Standard_D2_v2"
+    sqlStorageAccountType = "Standard_GRS"
+    dcStorageAccountType = "Standard_GRS"
+    webStorageAccountType = "Standard_GRS"
+    mgtStorageAccountType = "Standard_GRS"
+    templatesBaseUrl = "https://raw.githubusercontent.com/mosharafMS/GC-iaas-webapp/court/nestedtemplates"
+    dscBaseUrl = "https://raw.githubusercontent.com/mosharafMS/GC-iaas-webapp/court/DSC"
+    scriptsBaseUrl = "https://raw.githubusercontent.com/mosharafMS/GC-iaas-webapp/court/custom-scripts"
+    sqlServerVersion = "SQL2016-WS2012R2"
+    gatewaySkuName = "WAF_Medium"
+}
+
 
 $timestamp = Get-Date -Format "yyyy-MM-dd_hh-mm-ss"
-New-AzureRmResourceGroupDeployment -Name "D_$timestamp" -ResourceGroupName GCbluePrint `
--TemplateFile .\azuredeploy.json -TemplateParameterFile .\azuredeploy.parameters.json `
+New-AzureRmResourceGroupDeployment -Name "D_$timestamp" -ResourceGroupName GCbluePrintUser1 `
+-TemplateFile .\azuredeploy.json -TemplateParameterObject $parameters `
 -Mode Incremental -DeploymentDebugLogLevel ResponseContent -Verbose 
