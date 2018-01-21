7d8e15cf5036f3359c61b1da4cce4dcdf869b3d5
mosharafMS <Mohamed.Sharaf@Microsoft.com>
2018-01-21 01:47:43 -0500

    mosharaf. change parameter values for testing

--- deploy.ps1
+++ deploy.ps1
@@ -10,9 +10,9 @@ $subscriptionId = '3a4af7b3-b7ac-463d-9940-1d80445961a8'
 $parameters=@{
     workspaceRegion = "canadacentral"
     environmentName ="AzureCloud"
-    keyVaultName = "GCKeystoreUser2"
-    keyVaultResourceGroupName = "GCbluePrintUser2"
-    domainName = "GCblueprint.local"
+    keyVaultName = "GCBluePrint4321"
+    keyVaultResourceGroupName = "GCBluePrint"
+    domainName = "gc.local"
     configureSQLAO = "yes"
     environmentPrefix = "prod"
     numberOfWebInstances = 2
@@ -40,7 +40,7 @@ $timestamp = Get-Date -Format "yyyy-MM-dd_hh-mm-ss"
 #
 #
 #
-New-AzureRmResourceGroupDeployment -Name "D_$timestamp" -ResourceGroupName GCbluePrintUser2 `
+New-AzureRmResourceGroupDeployment -Name "D_$timestamp" -ResourceGroupName GCBluePrint `
 -TemplateFile .\azuredeploy.json -TemplateParameterObject $parameters `
--Mode Incremental -DeploymentDebugLogLevel ResponseContent -Verbose 
+-Mode Incremental -DeploymentDebugLogLevel ResponseContent  
 
