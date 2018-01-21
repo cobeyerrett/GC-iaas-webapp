44d92ce2f38c1c685f79eef623086fc69d076daa
aqasrawi <aqasrawi@microsoft.com>
2018-01-20 22:47:28 -0500

    NSG fix

--- deploy.ps1
+++ deploy.ps1
@@ -10,8 +10,8 @@ $subscriptionId = '3a4af7b3-b7ac-463d-9940-1d80445961a8'
 $parameters=@{
     workspaceRegion = "canadacentral"
     environmentName ="AzureCloud"
-    keyVaultName = "GCKeystoreUser3"
-    keyVaultResourceGroupName = "GCbluePrintUser3"
+    keyVaultName = "GCKeystoreUser1"
+    keyVaultResourceGroupName = "GCbluePrintUser1"
     domainName = "GCblueprint.local"
     configureSQLAO = "yes"
     environmentPrefix = "prod"
@@ -40,7 +40,7 @@ $timestamp = Get-Date -Format "yyyy-MM-dd_hh-mm-ss"
 #
 #
 #
-New-AzureRmResourceGroupDeployment -Name "D_$timestamp" -ResourceGroupName GCbluePrintUser3 `
+New-AzureRmResourceGroupDeployment -Name "D_$timestamp" -ResourceGroupName GCbluePrintUser1 `
 -TemplateFile .\azuredeploy.json -TemplateParameterObject $parameters `
 -Mode Incremental -DeploymentDebugLogLevel ResponseContent -Verbose 
 
