95d7fa759e76ca792d1e3ae65a0ab7257036150b
aqasrawi <aqasrawi@microsoft.com>
2018-01-25 14:19:32 -0500

    User2

--- deploy.ps1
+++ deploy.ps1
@@ -8,7 +8,7 @@
 $subscriptionId = '3a4af7b3-b7ac-463d-9940-1d80445961a8'
 Set-AzureRmContext -SubscriptionId $subscriptionId
 $timestamp = Get-Date -Format "yyyy-MM-dd_hh-mm-ss"
-$resourceGroupName = 'GCbluePrintUser1'
+$resourceGroupName = 'GCbluePrintUser2'
 
 #
 # Provision Basic Services / OMS Workspaces
@@ -45,9 +45,9 @@ New-AzureRmResourceGroupDeployment -Name "D_$timestamp" -ResourceGroupName $reso
 #
 # Everything else
 #
-#New-AzureRmResourceGroupDeployment -Name "D_$timestamp" -ResourceGroupName $resourceGroupName `
-#-TemplateFile .\azuredeploy05.json -TemplateParameterFile .\azuredeploy.parameters.json `
-#-Mode Incremental -DeploymentDebugLogLevel ResponseContent -Verbose 
+New-AzureRmResourceGroupDeployment -Name "D_$timestamp" -ResourceGroupName $resourceGroupName `
+-TemplateFile .\azuredeploy05.json -TemplateParameterFile .\azuredeploy.parameters.json `
+-Mode Incremental -DeploymentDebugLogLevel ResponseContent -Verbose 
 
 #
 # Encryption
@@ -55,8 +55,8 @@ New-AzureRmResourceGroupDeployment -Name "D_$timestamp" -ResourceGroupName $reso
 
 $aadClientID = 'c96ad29f-b88f-4c64-ac5f-fb7897b6826d'
 $aadClientSecret = '89ee108b-570b-44b6-8c7f-315b11d670a1'
-$diskEncryptionKeyVaultUrl = 'https://gckeystoreUser1.vault.azure.net/'
-$keyVaultResourceId = '/subscriptions/3a4af7b3-b7ac-463d-9940-1d80445961a8/resourceGroups/GCbluePrintUser1/providers/Microsoft.KeyVault/vaults/GCKeystoreUser1'
+$diskEncryptionKeyVaultUrl = 'https://gckeystoreUser2.vault.azure.net/'
+$keyVaultResourceId = '/subscriptions/3a4af7b3-b7ac-463d-9940-1d80445961a8/resourceGroups/GCbluePrintUser2/providers/Microsoft.KeyVault/vaults/GCKeystoreUser2'
 Set-AzureRmVMDiskEncryptionExtension -ResourceGroupName $resourceGroupName -VMName 'AZ-PDC-VMprod' -AadClientID $aadClientID -AadClientSecret $aadClientSecret -DiskEncryptionKeyVaultUrl $diskEncryptionKeyVaultUrl -DiskEncryptionKeyVaultId $keyVaultResourceId -VolumeType All -Verbose -Force
 Set-AzureRmVMDiskEncryptionExtension -ResourceGroupName $resourceGroupName -VMName 'AZ-BDC-VMprod' -AadClientID $aadClientID -AadClientSecret $aadClientSecret -DiskEncryptionKeyVaultUrl $diskEncryptionKeyVaultUrl -DiskEncryptionKeyVaultId $keyVaultResourceId -VolumeType All -Verbose -Force
 
