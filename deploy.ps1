af724df676f9fe65baf7636f14c1cca9d3084919
aqasrawi <aqasrawi@microsoft.com>
2018-01-26 14:06:38 -0500

    Removed old files

--- deploy.ps1
+++ deploy.ps1
@@ -5,7 +5,8 @@
 
 #Login-AzureRmAccount
 
-$subscriptionId = '3a4af7b3-b7ac-463d-9940-1d80445961a8'
+#$subscriptionId = '3a4af7b3-b7ac-463d-9940-1d80445961a8'
+$subscriptionId = '60b6165a-8669-47a2-860c-6ef475127364'
 Set-AzureRmContext -SubscriptionId $subscriptionId
 $timestamp = Get-Date -Format "yyyy-MM-dd_hh-mm-ss"
 $resourceGroupName = 'GCbluePrintUser2'
